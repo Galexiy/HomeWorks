1db8bcef27495de61efdd335357caef23075c745
Aleksey G <transaiv@mail.ru>
2022-10-23 14:59:56 +0300

    Выполнил ДЗ-1

---
+++ practice_1/Program.cs
@@ -0,0 +1,93 @@
+﻿// ДЗ 1
+
+
+// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+/*
+Console.Write("Imput a first number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Imput a second number: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+int min = b;
+
+if (a> max) max = a;
+if (b> max) max = b;
+if (a< max) min = a;
+if (b< max) min = b;
+
+Console.Write("max = ");
+Console.WriteLine(max);
+Console.Write("min = ");
+Console.WriteLine(min);
+*/
+
+
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+/*
+Console.Write("Imput a first number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Imput a second number: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Imput a second number: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+
+if (a> max) max = a;
+if (b> max) max = b;
+if (c> max) max = c;
+
+
+Console.Write("max = ");
+Console.WriteLine(max);
+*/
+
+// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+/*
+Console.Write("Imput a first number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+int index = a;
+
+if (a % 2 == 0)
+{
+    Console.WriteLine($"{a} это четное число");
+    
+}
+else
+{
+    Console.WriteLine($"{a} это нечетное число");
+}
+*/
+
+// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+/*
+Console.Write("Imput a first number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+    
+int current = 1;
+
+while(current <= n)
+{
+    if (current % 2 == 0)
+    Console.Write(current + "  ");
+    current++;
+}
+*/
+
